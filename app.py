d79a2f08394517b3dc5fafe0f5bf4b0c5d4df802
LusineGalstyann <lusinegal8@gmail.com>
2024-06-14 16:06:23 +0400

    Inheritance from base.html file. Incude bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__) #APP օբեյեկըի ստեղծեւմը Flask  դասից,  փոխանցում ենք հիմնական ֆայլի անունը
 
 @app.route("/")#դեկերատոր ,որը հետևում է URL-ում նշված էջը այս դեպքում՝ գլխավորը
+@app.route('/home')#նաև էս էջի համար կկանչի ինդեքս ֆունկիցան
 def index():
-    return "hi"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return "about page"
-if __name__=="__main__":#եթե այս ընթացիկ ֆայլն է հիմնական զապուսկի ֆայլը։Այսինքն էս ֆայլից ենք ռան տալիս էդ դեպքում __name__-ի մեջ կգրվի հենտ __main__
+    return render_template("about.html")
+
+@app.route("/user/<string:name>/<int:age>")
+def user(name,age):
+    return "User name: "+name +" he/she is :"+str(age)+" years old"
+if __name__=="__main__":#եթե այս ընթացիկ ֆայլն է հիմնական զապուսկի ֆայլը։Այսինքն էս ֆայլից ենք ռան տալիս էդ դեպքում __name__-ի մեջ կգրվի հետո __main__
     app.run(debug=True)
